d3ffb8c8269cff8180d4b4d2a4a56b01dfbb5cb4
Kunal-30 <kunal.tagde30@gmail.com>
2025-11-07 14:48:35 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Learning/', // 👈 Important: use your repo name here
+  base: '/Learning/', // 👈 MUST match your repo name
 })
