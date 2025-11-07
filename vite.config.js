8488aef836bec9958220781ac9c90f81e938afbb
Kunal-30 <kunal.tagde30@gmail.com>
2025-11-07 14:39:59 +0530

    Added vite.config.js and GitHub Pages config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Learning/', // 👈 Important: use your repo name here
+})
